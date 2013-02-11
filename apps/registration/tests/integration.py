22b5cc3129114ad76d0b47942de19e8792d755d0
Vincent <ohergal@gmail.com>
2013-02-12 00:32:46 +0800

    delete todos urls

--- apps/registration/tests/integration.py
+++ apps/registration/tests/integration.py
@@ -188,20 +188,29 @@ class RegisterTest(LiveServerTestCase):
         self.assertIsNotNone(user2)
 
 
+# class IndexViewTest(TestCase):
+#     def test_index_view(self):
+#         c = Client()
+#         response = c.get('/')
+#         self.assertEqual(response.status_code, 200)
+#         self.assertTemplateUsed('firstp/index.html')
+#         # title里有任务列表
+#         self.assertContains(response,
+#             '<title>首页 - 网络组内部业务系统</title>',
+#             status_code=200)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# # 首页的测试
+# class todosIndexViewTest(TestCase):
+#     def test_todos_view_index(self):
+#         """
+#         打开首页的时候,应该看见"任务列表",和新建任务.
+#         """
+#         c = Client()
+#         response = c.get('/todos/')
+#         self.assertEqual(response.status_code, 200)
+#         self.assertTemplateUsed('todos/index.html')
+#         # title里有任务列表
+#         self.assertContains(response,
+#             '<title>首页 - 任务列表 - 网络组内部业务系统</title>',
+#             status_code=200)
